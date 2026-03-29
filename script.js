011c3963dd21cec2807dc3cf77b2d754d3feea8b
ssurmik <139618243+ssurmik@users.noreply.github.com>
2026-03-29 15:23:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ const gifStages = [
 const noMessages = [
     "No",
     "Are you positive? 🤔",
-    "Pookie please... 🥺",
-    "If you say no, I will be really sad...",
-    "I will be very sad... 😢",
+    "Trixie please... 🥺",
+    "D kaba kakarmahin nyan te",
+    "Cge ganyan kana ha 😢",
     "Please??? 💔",
     "Don't do this to me...",
     "Last chance! 😭",
@@ -22,10 +22,10 @@ const noMessages = [
 ]
 
 const yesTeasePokes = [
-    "try saying no first... I bet you want to know what happens 😏",
-    "go on, hit no... just once 👀",
-    "you're missing out 😈",
-    "click no, I dare you 😏"
+    "Press No naman sayang effort HAHAHA",
+    "Hit No 👀",
+    "ganyan yan shaaaaa ha",
+    "click no, NGANI 😏"
 ]
 
 let yesTeasedCount = 0
